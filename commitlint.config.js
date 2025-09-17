a880ac8806df3eeb4c1e769fd78670a6b470b740
nhanbernie <ttnhanbernie@gmail.com>
2025-09-17 16:16:07 +0700

    chore: husky

---
+++ commitlint.config.js
@@ -0,0 +1,29 @@
+const configuration = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "build",
+        "ci",
+        "chore",
+        "revert",
+        "merge",
+        "config",
+        "deps",
+      ],
+    ],
+    "subject-case": [2, "never", ["sentence-case", "start-case", "pascal-case", "upper-case"]],
+    "header-max-length": [2, "always", 100],
+  },
+};
+
+export default configuration;
